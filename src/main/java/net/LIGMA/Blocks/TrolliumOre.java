de3146df42e93a674a96460177e4670e7d280def
rampagedoge <80089120+rampagedoge@users.noreply.github.com>
2021-06-23 18:45:39 -0400

    get a job

--- src/main/java/net/LIGMA/Blocks/TrolliumOre.java
+++ src/main/java/net/LIGMA/Blocks/TrolliumOre.java
@@ -5,10 +5,10 @@ import net.fabricmc.fabric.api.tools.FabricToolTags;
 import net.fabricmc.fabric.api.block.FabricBlockSettings;
 import net.minecraft.block.Material;
 
-public class TrolliumBlock extends Block {
+public class TrolliumOre extends Block {
 
-    public TrolliumBlock() {
-        super(FabricBlockSettings.of(Material.METAL).breakByHand(false).strength(25f,1500f).breakByTool(FabricToolTags.PICKAXES, 5).build());
+    public TrolliumOre() {
+        super(FabricBlockSettings.of(Material.METAL).breakByHand(false).strength(25f,1500f).breakByTool(FabricToolTags.PICKAXES, 4).build());
     }
 
 }
--- src/main/java/net/LIGMA/ModInit.java
+++ src/main/java/net/LIGMA/ModInit.java
@@ -3,6 +3,7 @@ package net.LIGMA;
 import net.LIGMA.Bases.PickaxeBase;
 import net.LIGMA.Bases.AxeBase;
 import net.LIGMA.Blocks.TrolliumBlock;
+import net.LIGMA.Blocks.TrolliumOre;
 import net.LIGMA.Materials.ToolMaterialTrollium;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.item.BlockItem;
@@ -22,6 +23,9 @@ public class ModInit implements ModInitializer {
     public static final Item TROLLIUM_PICKAXE = new PickaxeBase(new ToolMaterialTrollium());
     public static final Item TROLLIUM_AXE = new AxeBase(new ToolMaterialTrollium());
     public static final Item TROLLIUM_TOOL_PART = new Item(new Item.Settings().group(ItemGroup.MISC));
+    public static final Item RAW_TROLLIUM = new Item(new Item.Settings().group(ItemGroup.MISC));
+    public static final Block TROLLIUM_ORE = new TrolliumOre();
+    public static final BlockItem TROLLIUM_ORE_ITEM = new BlockItem(TROLLIUM_ORE, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS));
 
     private void RegisterItem(Identifier identity, Item item) {
         Registry.register(Registry.ITEM, identity, item); //strupid rergister
@@ -37,10 +41,15 @@ public class ModInit implements ModInitializer {
 
     @Override
     public void onInitialize() {
+        //Register Items
         RegisterItem(new Identifier(MODID, "trollium_ingot"), TROLLIUM_INGOT);
         RegisterItem(new Identifier(MODID, "trollium_tool_part"), TROLLIUM_TOOL_PART);
         RegisterItem(new Identifier(MODID, "trollium_pickaxe"), TROLLIUM_PICKAXE);
         RegisterItem(new Identifier(MODID, "trollium_axe"), TROLLIUM_AXE);
+        RegisterItem(new Identifier(MODID, "raw_trollium"), RAW_TROLLIUM);
+        //Register Blocks
+        //Register Blocks and Block Items
         RegisterBlockWithItem(new Identifier(MODID, "trollium_block"), TROLLIUM_BLOCK, new Identifier(MODID, "trollium_block"), TROLLIUM_BLOCK_ITEM);
+        RegisterBlockWithItem(new Identifier(MODID, "trollium_ore"), TROLLIUM_ORE, new Identifier(MODID, "trollium_ore"), TROLLIUM_ORE_ITEM);
     }
 }
