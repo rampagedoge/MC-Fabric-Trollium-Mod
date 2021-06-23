a9c7825969354154cca1005824451561b9c86971
rampagedoge <80089120+rampagedoge@users.noreply.github.com>
2021-06-23 16:19:29 -0400

    Added axe
    Added Trollium Axe
    Recipe
     ##
     c#
     c
    Repaired with Trollium_Ingots

---
+++ src/main/java/net/LIGMA/Bases/AxeBase.java
@@ -0,0 +1,14 @@
+package net.LIGMA.Bases;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.AxeItem;
+import net.minecraft.item.ToolMaterial;
+
+public class AxeBase extends AxeItem {
+
+    public AxeBase(ToolMaterial material) {
+        super(material, -1, -2.2f, new Item.Settings().group(ItemGroup.TOOLS));
+    }
+    
+}
